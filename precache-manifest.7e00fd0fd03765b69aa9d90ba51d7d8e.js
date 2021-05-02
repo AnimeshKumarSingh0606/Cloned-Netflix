0f818cf670824c48fe5bc8e06c7db0d8e235ec1d
Animesh Kumar Singh <animesh.2001singh@gmail.com>
2021-05-02 22:38:32 +0530

    Updates

---
+++ precache-manifest.7e00fd0fd03765b69aa9d90ba51d7d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0144c0d6836a88f5634bc9064ba1409b",
+    "url": "/Cloned-Netflix/index.html"
+  },
+  {
+    "revision": "a67880ac695956b4bf2a",
+    "url": "/Cloned-Netflix/static/css/main.2f6e5d85.chunk.css"
+  },
+  {
+    "revision": "0c983c0bc837ce4d158b",
+    "url": "/Cloned-Netflix/static/js/2.47d130ce.chunk.js"
+  },
+  {
+    "revision": "a435a3ec762d9159b1230d9b6e18884c",
+    "url": "/Cloned-Netflix/static/js/2.47d130ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67880ac695956b4bf2a",
+    "url": "/Cloned-Netflix/static/js/main.e5822c71.chunk.js"
+  },
+  {
+    "revision": "a30f435fc138d44131eb",
+    "url": "/Cloned-Netflix/static/js/runtime-main.2f84ec2c.js"
+  }
+]);
